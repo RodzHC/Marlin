477591b2f87482ca4a7248edf99d64d6b75406e7
Rodrigo M. Marzullo <rodrigo.marzullo@gmail.com>
2019-09-02 17:36:43 -0300

    SD + Temp
    Add SD Card + Temp Control for OverHeat

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1329,7 +1329,7 @@
  * you must uncomment the following option or it won't work.
  *
  */
-//#define SDSUPPORT
+#define SDSUPPORT
 
 /**
  * SD CARD: SPI SPEED
@@ -1346,7 +1346,7 @@
  *
  * Use CRC checks and retries on the SD communication.
  */
-//#define SD_CHECK_AND_RETRY
+#define SD_CHECK_AND_RETRY
 
 //
 // ENCODER SETTINGS
